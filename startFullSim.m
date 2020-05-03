638d8cbcd878b585b9b826b7d5fcefcbc8bfde4f
kevinmm6 <kevinmm@chem.ubc.ca>
2020-05-03 12:15:28 -0700

    Fixed sigma parameter

--- startFullSim.m
+++ startFullSim.m
@@ -1,4 +1,4 @@
-function [time,nden,eden,deac_n_min,deac_dr,deac_pd,Te,rx,ry,rz,vx,vy,vz,vol,y0] = startFullSim(density,n,N,t_max,steps,dirname,name)
+function [time,nden,eden,deac_n_min,deac_dr,deac_pd,Te,rx,ry,rz,vx,vy,vz,vol,y0] = startFullSim(density,n,N,t_max,steps,dirname,name,sigma_z,sigma_x)
 addToPath = pwd;
 addpath(addToPath);
 %STARTFULLSIM Run this to see the bifurcation model                                           
@@ -10,8 +10,8 @@ addpath(addToPath);
         %     sigma_x=0.70*1000;
         %     t_max = max time in ns
         %     steps = how many time steps
-              sigma_z=1*1000; %Gaussian width in um
-             sigma_x=0.70*1000; %um
+%               sigma_z=1*1000; %Gaussian width in um
+%              sigma_x=0.70*1000; %um
 %states = 100;
 
 [time,nden,eden,deac_n_min,deac_dr,deac_pd,Te,rx,ry,rz,vx,vy,vz,vol,y0] = start_sim_fun(density,N,n,sigma_z,sigma_x,t_max,steps,dirname,name,false);
