a40866c6b40bbb0af1ffa0c1ab4a9f5b3988fada
kevinmm6 <kevinmm@chem.ubc.ca>
2020-05-23 12:02:44 -0700

    Beggining

---
+++ penningIonization.m
@@ -0,0 +1,35 @@
+function [T_PENNING,deac,D_DEAC_N_MIN,nden,NL,DEN0,NDEN,EDEN,DEAC]= penningIonization(ns,n_min,n0,den0,N,firstn,nl,Ry,kB)
+% Sets initial conditions for electron and n-level distributions:
+% no initial electrons -> calc. Penning seed electrons (look up Robicheaux)
+penningByShell = zeros(N,1);
+T_PENNING=zeros(1,N);
+D_DEAC_N_MIN = zeros(n_min,N);
+for ii=1:N %loop though all shells
+    h=1+(ii-1)*ns;
+    k=ii*ns;
+    hh=1+(ii-1)*n_min;
+    kk=ii*n_min;
+    
+    [PF,eden,rden]=penningfraction(n0,den0(ii));
+    penningByShell(ii,1) = PF;
+    % Redistributes the Penning partners over lower n's:
+    f=@(x)5.95*x.^5;                % This is the penning fraction distribution
+    np=firstn:fix(n0/sqrt(2));      % Array of n states allowed after Penn ion
+    ind=1:length(np);               % This is the distribution of penning fraction
+    nden=nl*0;
+    nden(ind)=eden*f(np/n0)/sum(f(np/n0));  % dividing by the sum normalizes the function
+    
+    nden(nl==n0)=rden;              % set n0 to rden
+    
+    % Set initial temperature:    (Robicheaux 2005 JPhysB)
+    T_PENNING(ii)=(-Ry*den0(ii)/n0^2 + Ry*rden/n0^2 + Ry*sum(nden(ind)./nl(ind).^2) )*1/(3/2*kB*eden); % by energy conservation
+    deac=sum(nden(1:n_min));    % allow n<=n_min to decay
+    D_DEAC_N_MIN(:,ii)=nden(1:n_min);
+    nden(1:n_min)=zeros(n_min,1);
+   
+    NL(:,ii)=nl;       % save values for this shell in arrays
+    DEN0(:,ii)=repmat(den0(ii),ns,1);
+    NDEN(:,ii)=nden;
+    EDEN(ii)=eden;
+    DEAC(ii)=deac;
+end
--- shell_rate_eqn_sim.m
+++ shell_rate_eqn_sim.m
@@ -22,8 +22,6 @@
 
 function [t,nden,eden,deac_n_min,deac_dr,deac_pd,Te,r_x,r_y,r_z,v_x,v_y,v_z,v,y0]=shell_rate_eqn_sim(den0, rx ,ry, rz ,n0, numTimeSteps, t_final, single)
 
-killPenning = false;
-
 %%%%%%%%%%%%%%%%%%%%%%
 % Initial conditions %
 %%%%%%%%%%%%%%%%%%%%%%
@@ -53,7 +51,6 @@ DEAC=zeros(N,1);
 DEAC_DR=zeros(N,1);
 DEAC_PD=zeros(ns,N);
 DEAC_N_MIN=zeros(n_min,N);
-T_PENNING=zeros(1,N);
 
 ux=zeros(size(rx));
 uz=zeros(size(rx));
@@ -63,48 +60,7 @@ volume= 4/3*pi* (rx.*ry.*rz - [0; rx(1:end-1)].*[0; ry(1:end-1)].*[0; rz(1:end-1
 
 tspan= linspace(0,t_final,numTimeSteps);
 
-% Sets initial conditions for electron and n-level distributions:
-% no initial electrons -> calc. Penning seed electrons (look up Robicheaux)
-penningByShell = zeros(N,1);
-for ii=1:N %loop though all shells
-    h=1+(ii-1)*ns;
-    k=ii*ns;
-    hh=1+(ii-1)*n_min;
-    kk=ii*n_min;
-    
-    [PF,eden,rden]=penningfraction(n0,den0(ii));
-    penningByShell(ii,1) = PF;
-    % Redistributes the Penning partners over lower n's:
-    f=@(x)5.95*x.^5;                % This is the penning fraction distribution
-    np=firstn:fix(n0/sqrt(2));      % Array of n states allowed after Penn ion
-    ind=1:length(np);               % This is the distribution of penning fraction
-    nden=nl*0;
-    nden(ind)=eden*f(np/n0)/sum(f(np/n0));  % dividing by the sum normalizes the function
-    
-    nden(nl==n0)=rden;              % set n0 to rden
-    
-    % Set initial temperature:    (Robicheaux 2005 JPhysB)
-    T_PENNING(ii)=(-Ry*den0(ii)/n0^2 + Ry*rden/n0^2 + Ry*sum(nden(ind)./nl(ind).^2) )*1/(3/2*kB*eden); % by energy conservation
-    
-    if (killPenning)
-        deac=sum(nden(1:n0-1));    % allow n<n0 to decay
-        D_DEAC_N_MIN(:,ii)=nden(1:n0-1);
-        nden(1:n0-1)=zeros(n0-1,1);
-    else
-        deac=sum(nden(1:n_min));    % allow n<=n_min to decay
-        D_DEAC_N_MIN(:,ii)=nden(1:n_min);
-        nden(1:n_min)=zeros(n_min,1);
-    end
-    
-    
-    NL(:,ii)=nl;       % save values for this shell in arrays
-    DEN0(:,ii)=repmat(den0(ii),ns,1);
-    NDEN(:,ii)=nden;
-    EDEN(ii)=eden;
-    DEAC(ii)=deac;
-    
-    
-end
+[T_PENNING,deac,D_DEAC_N_MIN,nden,NL,DEN0,NDEN,EDEN,DEAC]= penningIonization(ns,n_min,n0,den0,N,firstn,nl,Ry,kB);
 
 
 %the penning temperature is equal for all densities! (density scaling factor cancels)
