c36783b88b8d58351bf9b5f218fc59ea21162ba7
Kiara <kiarag@alumni.ubc.ca>
2020-05-26 14:28:38 -0700

    more testing needed but maybe good

--- penningIonization.m
+++ penningIonization.m
@@ -1,7 +1,6 @@
 function [T_PENNING,deac,D_DEAC_N_MIN,nden,NL,DEN0,NDEN,EDEN,DEAC]= penningIonization(ns,n_min,n0,den0,N,firstn,nl,Ry,kB,penningTimes)
 % Sets initial conditions for electron and n-level distributions:
 % no initial electrons -> calc. Penning seed electrons (look up Robicheaux)
-penningByShell = zeros(N,1);
 T_PENNING=zeros(1,N);
 D_DEAC_N_MIN = zeros(n_min,N);
 EDEN = zeros(N,1);
@@ -11,23 +10,44 @@ for ii=1:N %loop though all shells
     k=ii*ns;
     hh=1+(ii-1)*n_min;
     kk=ii*n_min;
-   
-    for jj = 1:penningTimes
-        [PF,eden,rden]=penningfraction(n0,den0(ii));
-        % Redistributes the Penning partners over lower n's:
-        f=@(x)5.95*x.^5;                % This is the penning fraction distribution
-        np=firstn:fix(n0/sqrt(2));      % Array of n states allowed after Penn ion
-        ind=1:length(np);               % This is the distribution of penning fraction
-        nden=nl*0;
-        nden(ind)=eden*f(np/n0)/sum(f(np/n0));  % dividing by the sum normalizes the function
-        nden(nl==n0)=rden;              % set n0 to rden
-    end
+    nden=nl*0;
+    f=@(x)5.95*x.^5;                % This is the penning fraction distribution
+    
+    [PF,primaryEden,primaryRden]=penningfraction(n0,den0(ii));
+    % Redistributes the Penning partners over lower n's:  
+    np=firstn:fix(n0/sqrt(2));      % Array of n states allowed after Penn ion
+    ind=1:length(np);               % This is the distribution of penning fraction
+    % dividing by the sum normalizes the function,
+    % multiplying by eden gives it the correct magnitude
+    nden(ind) = nden(ind) + (primaryEden*f(np/n0)/sum(f(np/n0)))';
+    nden(nl==n0)= primaryRden;              % set n0 to rden
+    
+    
+    penningPartnerProportion = primaryEden/(primaryEden+primaryRden);
+    remainingProportion = primaryRden/(primaryEden+primaryRden);
+    spMean = fix(n0/sqrt(2));
+    n_avg = fix((spMean * penningPartnerProportion) + (n0 * remainingProportion));
+    combinedDen = den0(ii) - primaryEden;
+    [PF, secondaryEden, secondaryRden] = penningfraction(n_avg, combinedDen);
+    
+    % multiply by secondaryEden, scaled by which proportion were n0 gives it the correct magnitude
+    nden(ind)= nden(ind) + (secondaryEden*remainingProportion*f(np/n0)/sum(f(np/n0)))';
+    nden(ind)= nden(ind) - (secondaryEden*penningPartnerProportion*f(np/n0)/sum(f(np/n0)))';
+    nden(nl==n0) = nden(nl==n0) - secondaryEden*remainingProportion;
+    
+    secondaryPenningN=firstn:fix(spMean/sqrt(2));      % Array of n states allowed after Penn ion
+    spInd = 1:length(secondaryPenningN);
+    secondaryPenningDist = f(secondaryPenningN/spMean)/sum(f(secondaryPenningN/spMean));
+    nden(spInd) = nden(spInd) + (secondaryEden*penningPartnerProportion*secondaryPenningDist)';
+    
+    
+    
     % Set initial temperature:    (Robicheaux 2005 JPhysB)
     T_PENNING(ii)=(-Ry*den0(ii)/n0^2 + Ry*rden/n0^2 + Ry*sum(nden(ind)./nl(ind).^2) )*1/(3/2*kB*eden); % by energy conservation
     deac=sum(nden(1:n_min));    % allow n<=n_min to decay
     D_DEAC_N_MIN(:,ii)=nden(1:n_min);
     nden(1:n_min)=zeros(n_min,1);
-   
+    
     NL(:,ii)=nl;       % save values for this shell in arrays
     DEN0(:,ii)=repmat(den0(ii),ns,1);
     NDEN(:,ii)=nden;
